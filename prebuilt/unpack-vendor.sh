4f0dbb45d81f063f85560ebcf030c25c90206bb4
spector0 <www.ranjith007.ra@gmail.com>
2022-10-22 07:00:25 +0530

    add prebuilt vendor with proper compression

--- prebuilt/unpack-vendor.sh
+++ prebuilt/unpack-vendor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ROOT=$(dirname "$(readlink -f "$0")")
 echo "Joining the fragments..."
-cat $ROOT/fragments/vendor.a* > $ROOT/vendor.img.xz
+cat $ROOT/fragments/vendor.img.xz.a* > $ROOT/vendor.img.xz
 echo "Unpacking..."
 unxz $ROOT/vendor.img.xz
 echo "Done!"
